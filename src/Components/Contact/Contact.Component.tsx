a421c80f8724ca5cd6373641eead8741a83dc6ba
Vimal Raveendran <vimalraveendra@gmail.com>
2025-02-14 10:29:11 +0100

    added ContactComponent,ButtonComponent & its styles

---
+++ src/Components/Contact/Contact.Component.tsx
@@ -0,0 +1,12 @@
+
+import "./Contact.styles.css"
+
+const ContactComponent = () => {
+  return (
+    <div className="contact-main-container">
+        Contact Component
+    </div>
+  )
+}
+
+export default ContactComponent
