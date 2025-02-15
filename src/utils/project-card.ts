76aa38b7ce0b70da161f29df5e654ec038cb34b3
Vimal Raveendran <vimalraveendra@gmail.com>
2025-02-15 15:38:14 +0100

    updated the path of images

--- src/utils/project-card.ts
+++ src/utils/project-card.ts
@@ -1,18 +1,28 @@
 import { IProjectCard } from "../Model/Portfolio.model";
+import Tasks from "../assets/tasks-management.png";
+import Magic from "../assets/magic-match.png";
+import Weather from "../assets/weather.png";
+import Recipe from "../assets/recipe.png";
+import Customer from "../assets/list.png";
+import Movie from "../assets/movie-finder.png";
+import Calculator from "../assets/investment-calculator.png";
+import Robo from "../assets/robo-friends.png";
+import Shopping from "../assets/shopping-list.png";
+import Generator from "../assets/background-generator.png";
     
 
 export const CARD_DATA:IProjectCard[]=[
     {
         id:100,
         link:"https://vimalraveendra.github.io/tasks-management-ngrx/",
-        image:"./src/assets/tasks-management.png",
+        image:Tasks,
         name:'tasks-management'
     },
     {
         id:101,
         link:"https://vimalraveendra.github.io/React-vite-task/",
         image:"./src/assets/magic-match.png",
-        name:'magic-match'
+        name:Magic,
     },
     
    
@@ -20,52 +30,52 @@ export const CARD_DATA:IProjectCard[]=[
         id:102,
         link:"https://vimalraveendra.github.io/weatherapp-redux/",
         image:"./src/assets/weather.png",
-        name:'weather-finder'
+        name:Weather,
     },
     {
         id:103,
         link:"https://vimalraveendra.github.io/food-recipe/",
         image:"./src/assets/recipe.png",
-        name:'recipe-search'
+        name:Recipe
     },
     {
         id:104,
         link:"https://vimalraveendra.github.io/personslist-redux/",
         image:"./src/assets/list.png",
-        name:'customer-details'
+        name:Customer
     },
    
     {
         id:105,
         link:"https://vimalraveendra.github.io/movie-finder-angular/",
         image:"./src/assets/movie-finder.png",
-        name:'movie-finder'
+        name:Movie,
     },
     {
         id:106,
         link:"https://vimalraveendra.github.io/investment-calculator-standalone/",
         image:"./src/assets/investment-calculator.png",
-        name:'investment-calculator'
+        name:Calculator,
     },
   
     {
         id:107,
         link:"https://vimalraveendra.github.io/RoboFriend-Redux/",
         image:"./src/assets/robo-friends.png",
-        name:'robo-friends'
+        name:Robo,
     },
     {
         id:108,
         link:"https://vimalraveendra.github.io/shoppingList/",
         image:"./src/assets/shopping-list.png",
-        name:'shopping-list'
+        name:Shopping,
     },
    
     {
         id:109,
         link:"https://vimalraveendra.github.io/Background-Generator/",
         image:"./src/assets/background-generator.png",
-        name:'background-generator'
+        name:Generator,
     },
    
 ]
